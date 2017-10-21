2fea5ed40cef33b595220f0080b2b999269f4a90
HennieBumms <hzabel@itbh.local>
2017-10-21 19:16:19 +0200

    small score changes

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -3,10 +3,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BulletController : MonoBehaviour {
 
-    
+    public Text text;
     
     // Use this for initialization
 	void Start () {
