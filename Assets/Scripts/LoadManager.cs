f0c950d81d7919c430c32a0910c0680e9eaf4b2f
IT-Junkie-Maddin <32967231+IT-Junkie-Maddin@users.noreply.github.com>
2017-10-22 00:53:18 +0200

    start3

---
+++ Assets/Scripts/LoadManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadManager : MonoBehaviour
+{
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        print("quit");
+    }
+}
