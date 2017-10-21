29699b32de701f00c3be4d4db3afe5f82bd7ff78
André Hauser <a.hauser@neusta.de>
2017-10-21 13:02:40 +0200

    Add child movement

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -6,11 +6,17 @@ public class Main : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        Screen.SetResolution(1024, 768, true, 60);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// here goes main rendering
+
 	}
+
+    private void FixedUpdate()
+    {
+        // here goes render for fixed rates
+    }
 }
