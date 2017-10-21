f11018f1471c37ed603ef774ac4e239da7441b32
IT-Junkie-Maddin <32967231+IT-Junkie-Maddin@users.noreply.github.com>
2017-10-21 17:06:13 +0200

    pills

--- Assets/Scripts/LifebarController.cs
+++ Assets/Scripts/LifebarController.cs
@@ -7,8 +7,8 @@ public class LifebarController : MonoBehaviour
 {
 
     public LifepointsController lifepointsController;
-    float lifebarYSize;
-    float r;
+    private float lifebarYSize;
+    private float r;
 
     // Use this for initialization
     void Start()
@@ -21,7 +21,7 @@ public class LifebarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float lifebarYSize = lifepointsController.GetLifepoints();
+        lifebarYSize = lifepointsController.GetLifepoints();
         GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, lifebarYSize);
     }
 
