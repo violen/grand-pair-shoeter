10c7dfe8af1a47efee91bda64842636ea27a222e
HennieBumms <hzabel@itbh.local>
2017-10-21 17:45:54 +0200

    Bullets destroy children
    Bullets should now drestroy children with "Child" tag

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Child"){ 
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
