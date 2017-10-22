a13193938e68c180899992514527d3fbc7aacb0f
IT-Junkie-Maddin <32967231+IT-Junkie-Maddin@users.noreply.github.com>
2017-10-22 15:31:45 +0200

    gameover

---
+++ Assets/Scripts/RestartButtonAnime.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RestartButtonAnime : MonoBehaviour {
+
+        public MaskableGraphic restartButton;
+        
+        public float interval = 1f;
+        public float startDelay = 0.5f;
+        public bool currentState = true;
+        public bool defaultState = true;
+        bool isBlinking = false;
+       
+
+
+	// Use this for initialization
+	void Start () {
+        restartButton.enabled = defaultState;
+        StartBlink();
+	}
+
+    public void StartBlink()
+    {
+        if (isBlinking)
+            return;
+        if (restartButton != null)
+        {
+            isBlinking = true;
+            InvokeRepeating("ToggleState", startDelay, interval);
+        }
+    }
+	
+    public void ToggleState()
+    {
+        restartButton.enabled = !restartButton.enabled;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
